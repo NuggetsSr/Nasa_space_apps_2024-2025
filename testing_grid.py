25fbb82a9331529773138801f861dab69579b13f
NuggetsSr <ericcai1219@gmail.com>
2024-10-05 11:22:23 -0400

    checkpoint_1

--- testing_grid.py
+++ testing_grid.py
@@ -5,5 +5,5 @@ with open('PS_2024.10.05_06.28.35.csv', mode='r') as file:
     csv_reader = csv.reader(file)
     # Iterate over rows in the CSV
     for row in csv_reader:
-        print(row)
+        print(row[0])
         print()
