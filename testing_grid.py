68eb35101d001204c1a2a39433ab33317608754e
NuggetsSr <ericcai1219@gmail.com>
2024-10-05 11:36:32 -0400

    checkpoint_3

--- testing_grid.py
+++ testing_grid.py
@@ -7,7 +7,7 @@ file = open('habitable exoplanets list.txt', 'r')
 content = file.read()
 
 
-with open('PS_2024.10.05_06.28.35.csv', mode='r') as f:
+with open('PSCompPars_2024.10.05_07.42.51.csv', mode='r') as f:
     csv_reader = csv.reader(f)
     # Iterate over rows in the CSV
     for row in csv_reader:
