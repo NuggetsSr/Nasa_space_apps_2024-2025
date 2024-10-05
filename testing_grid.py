b8ce5c7b5f3b265cacbf3d577e331c5547013918
NuggetsSr <ericcai1219@gmail.com>
2024-10-05 10:56:01 -0400

    test

---
+++ testing_grid.py
@@ -0,0 +1,9 @@
+import csv
+
+# Open and read a CSV file
+with open('PS_2024.10.05_06.28.35.csv', mode='r') as file:
+    csv_reader = csv.reader(file)
+    # Iterate over rows in the CSV
+    for row in csv_reader:
+        print(row)
+        print()
